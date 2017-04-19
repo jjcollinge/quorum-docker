ae903479980acd904ff6a5ed7c2a796c00a1cdf0
Jonathan Collinge <jonathancollinge@live.com>
2017-04-19 20:29:17 +0100

    Fixed log naming issue

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 UNIQUE_ID=$(date +%s)
-LOG="run${UNIQUE_ID}.log"
+LOG="run-${UNIQUE_ID}.log"
 
 if [ ! -d "target" ]; then
         BUILD_IMAGE_ID="build${UNIQUE_ID}"
         BUILD_INSTANCE_ID="${BUILD_IMAGE_ID}-container"
-        echo "Building image ${BUILD_IMAGE_ID}">>LOG
+        echo "Building image ${BUILD_IMAGE_ID}">>${LOG}
         docker build -t $BUILD_IMAGE_ID -f Dockerfile.build .
-        echo "Creating container ${BUILD_INSTANCE_ID}">>LOG
+        echo "Creating container ${BUILD_INSTANCE_ID}">>${LOG}
         docker run --name $BUILD_INSTANCE_ID $BUILD_IMAGE_ID
         mkdir -p ./target
         docker cp $BUILD_INSTANCE_ID:quorum/build/bin/bootnode ./target/bootnode
@@ -17,14 +17,14 @@ if [ ! -d "target" ]; then
         docker cp $BUILD_INSTANCE_ID:ubuntu1604/constellation-node ./target/constellation-node
         docker rm $BUILD_INSTANCE_ID
 else
-        echo "Artefacts in target folder already exist, skipping build">>LOG
+        echo "Artefacts in target folder already exist, skipping build">>${LOG}
 fi
 
 SERVICE_IMAGE_ID="service${UNIQUE_ID}"
 SERVICE_CONTAINER_ID="${SERVICE_IMAGE_ID}-container"
-echo "Build image ${SERVICE_IMAGE_ID}">>LOG
+echo "Build image ${SERVICE_IMAGE_ID}">>${LOG}
 docker build --no-cache -t $SERVICE_IMAGE_ID -f Dockerfile .
-ARGS="--rm -it -e BOOTNODE_ADDRESS="$BOOTNODE_ADDRESS" -p 33445:33445 -p 8545:8545 -p 30303:30303 $SERVICE_IMAGE_ID"
-echo "Creating container ${SERVICE_CONTAINER_ID}">>LOG
-echo "Args: ${ARGS}">>LOG
+ARGS="--rm -it -e BOOTNODE_ADDRESS=${BOOTNODE_ADDRESS} -p 33445:33445 -p 8545:8545 -p 30303:30303 ${SERVICE_IMAGE_ID}"
+echo "Creating container ${SERVICE_CONTAINER_ID}">>${LOG}
+echo "Args: ${ARGS}">>${LOG}
 docker run --name $SERVICE_CONTAINER_ID $ARGS
