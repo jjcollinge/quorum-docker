a64ba9894eca9620cbe11be553e96661ab0f45a0
Jonathan Collinge <jonathancollinge@live.com>
2017-04-20 10:26:51 +0100

    Allowed dynamic node-configs to be injected

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,7 +3,26 @@
 UNIQUE_ID=$(date +%s)
 LOG="run-${UNIQUE_ID}.log"
 
-if [ ! -d "target" ]; then
+if [ ! -v "PATH_TO_NODE" ];
+then
+        echo "PATH_TO_NODE not set, please provide a valid node config">>${LOG}
+        exit 1;
+elif [ ! -d "${PATH_TO_NODE}" ];
+then
+        echo "PATH_TO_NODE set, but ${PATH_TO_NODE} directory doesn't exist">>${LOG}
+        exit 1;
+else
+        echo "Copying node-config to local directory"
+        rm -rf node-config
+        mkdir -p node-config
+        cp $PATH_TO_NODE node-config
+fi
+
+if [ ! -f "./target/bootnode" ] &&
+   [ ! -f "./target/geth" ] &&
+   [ ! -f "./target/constellation-enclave-keygen" ] &&
+   [ ! -f "./target/constellation-node" ];
+then
         BUILD_IMAGE_ID="build${UNIQUE_ID}"
         BUILD_INSTANCE_ID="${BUILD_IMAGE_ID}-container"
         echo "Building image ${BUILD_IMAGE_ID}">>${LOG}
@@ -17,14 +36,14 @@ if [ ! -d "target" ]; then
         docker cp $BUILD_INSTANCE_ID:ubuntu1604/constellation-node ./target/constellation-node
         docker rm $BUILD_INSTANCE_ID
 else
-        echo "Artefacts in target folder already exist, skipping build">>${LOG}
+        echo "Binaries already in target/ folder, to force a rebuild, stop this command and re-run after deleting the existing target/ folder">>${LOG}
 fi
 
 SERVICE_IMAGE_ID="service${UNIQUE_ID}"
 SERVICE_CONTAINER_ID="${SERVICE_IMAGE_ID}-container"
 echo "Build image ${SERVICE_IMAGE_ID}">>${LOG}
 docker build --no-cache -t $SERVICE_IMAGE_ID -f Dockerfile .
-ARGS="--rm -it -e BOOTNODE_ADDRESS=${BOOTNODE_ADDRESS} -e CONSTELLATION_PORT=${CONSTELLATION_PORT} -p 33445:33445 -p 8545:8545 -p 30303:30303 -p ${CONSTELLATION_PORT}:${CONSTELLATION_PORT} ${SERVICE_IMAGE_ID}"
+ARGS="--rm -it -e BOOTNODE_ADDRESS=${BOOTNODE_ADDRESS} -e PATH_TO_NODE=${PATH_TO_NODE} -p 33445:33445 -p 8545:8545 -p 30303:30303 -p 9000:9000 ${SERVICE_IMAGE_ID}"
 echo "Creating container ${SERVICE_CONTAINER_ID}">>${LOG}
 echo "Args: ${ARGS}">>${LOG}
 docker run --name $SERVICE_CONTAINER_ID $ARGS
