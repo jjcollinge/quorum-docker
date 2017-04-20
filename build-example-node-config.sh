b0f74078a9ca86975bc2d799bf822d643eadf730
Jonathan Collinge <jonathancollinge@live.com>
2017-04-20 12:04:56 +0100

    Changed CURL urls to raw urls

--- build-example-node-config.sh
+++ build-example-node-config.sh
@@ -9,16 +9,16 @@ curl -O https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/e
 mkdir -p keys
 
 # Get example key files
-curl https://github.com/jpmorganchase/quorum-examples/blob/master/examples/7nodes/keys/tm1.key -O
+curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/keys/tm1.key -O
 mv tm1.key node.key
 
-curl https://github.com/jpmorganchase/quorum-examples/blob/master/examples/7nodes/keys/tm1.pub -O
+curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/keys/tm1.pub -O
 mv tm1.pub node.pub
 
-curl https://github.com/jpmorganchase/quorum-examples/blob/master/examples/7nodes/keys/tm1a.key -O
+curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/keys/tm1a.key -O
 mv tm1a.key nodea.key
 
-curl https://github.com/jpmorganchase/quorum-examples/blob/master/examples/7nodes/keys/tm1a.pub -O
+curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/keys/tm1a.pub -O
 mv tm1a.pub nodea.pub
 
 curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/keys/key1 -O
@@ -26,3 +26,4 @@ curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/exam
 # Get example conf
 curl https://raw.githubusercontent.com/jpmorganchase/quorum-examples/master/examples/7nodes/tm1.conf -O
 mv tm1.conf node.conf
+sed -i -e s/tm1/node/g node.conf
